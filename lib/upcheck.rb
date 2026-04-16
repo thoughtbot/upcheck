77dfc3ba9dbe896f0e5bd421f4ea33c833ca93cd
Matheus Richard <matheusrichardt@gmail.com>
2026-04-16 18:33:29 -0300

    Let there be code!

---
+++ lib/upcheck.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "upcheck/version"
+
+module Upcheck
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/upcheck_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Upcheck do
+  it "has a version number" do
+    expect(Upcheck::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
